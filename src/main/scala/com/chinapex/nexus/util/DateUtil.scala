5643be1f861c671265dc970b775aa4bb89cb8fce
pengmingguo <mingguopeng@gmail.com>
2018-02-08 15:59:59 +0800

    1. init project

---
+++ src/main/scala/com/chinapex/nexus/util/DateUtil.scala
@@ -0,0 +1,10 @@
+package com.chinapex.nexus.util
+
+/**
+  * created by pengmingguo on 2/8/18
+  */
+object DateUtil {
+
+  def a = println(123)
+
+}
---
+++ src/test/scala/com/chinapex/test/Test1.scala
@@ -0,0 +1,15 @@
+package com.chinapex.test
+
+import org.junit.Test
+
+/**
+  * created by pengmingguo on 2/8/18
+  */
+class Test1 {
+
+  @Test
+  def a : Unit = {
+    println(123)
+  }
+
+}
